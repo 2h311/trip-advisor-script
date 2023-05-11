d9edfe6258fe8b631e123cb811cc6953619959e0
2h311 <kcodepal@outlook.com>
2023-05-11 13:54:23 -0500

    i think I just started working on this

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # trip-advisor-script
 Crawler for TripAdvisor
+
+Crawler for TripAdvisor
+
+Hi all, I need an application in nodejs that is capable of
+1) Open the tripadvisor site
+2) Select one of the sections of the website (hotel, restaurant, etc etc) (this value is dinamic, need to pass when call application)
+3) In the filter insert the region / city (this value is dinamic, need to pass when call application)
+4) Search result
+5) Open each result (like open each restaurant) and save all informations, to be precise:
+Business name, address, all the images in the gallery (saved in base64, contacts (telephone, website, etc), type of cuisine, details
+6) Save all this information in the database, you will need to create the structure of the databaes and share it with me before starting,
+there will be dedicated tables such as address, photographs, contacts, etc.
+
+The script will finish when it has saved all the restaurants of the selected region in the database.
+
+Important: if you call it twice, the script will have to update the structure and not create a duplicate.
---
+++ hotels.py
@@ -0,0 +1,69 @@
+import logging
+from pathlib import Path
+from typing import Optional
+from typing import Generator
+
+from playwright.sync_api import sync_playwright
+from playwright.sync_api import TimeoutError as PlaywrightTimeoutError
+from playwright.sync_api._generated import Page
+from playwright.sync_api._generated import BrowserType
+from playwright.sync_api._generated import ElementHandle
+
+
+logging.basicConfig(format="... %(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+BROWSER_TIMEOUT = 45 * 1000
+TRIP_ADVISOR_HOMEPAGE = "https://www.tripadvisor.com"
+
+
+def get_page_object(browser: BrowserType, proxies_pool: Optional[Generator] = None):
+    if proxies_pool:
+        proxy = next(proxies_pool)
+        proxy_dict = {
+            "server": f"http://{proxy.get('host').strip()}:{proxy.get('port').strip()}"
+        }
+
+        if len(proxy) == 4:
+            proxy_dict["username"] = proxy.get("username").strip()
+            proxy_dict["password"] = proxy.get("password").strip()
+        context = browser.new_context(proxy=proxy_dict)
+    else:
+        context = browser.new_context()
+
+    page = context.new_page()
+    page.set_default_timeout(BROWSER_TIMEOUT)
+    page.set_default_navigation_timeout(BROWSER_TIMEOUT)
+    return page
+
+
+# @retry_wraps()
+def goto_url(url: str, page: Page, page_load_state: str = "load") -> None:
+    logger.info(f"Visiting the url -> {url}")
+    page.wait_for_load_state(page_load_state)
+    response = page.goto(url)
+    if response.ok:
+        logger.debug("Page Done Loading...")
+
+
+def get_file_content(filename: str):
+    # check if we have an accounts.csv file
+    if not Path(filename).exists():
+        filename = Path(input("\aEnter a valid filename: "))
+        # if user inputs a non existing filename, raise an exception
+        if not Path(filename).exists():
+            raise Exception("\aYou might have to check the file name.")
+
+    with open(filename, "r") as fp:
+        content = [text.strip() for text in fp]
+    return content
+
+
+# playwright = sync_playwright().start()
+# browser = playwright.chromium.launch(headless=False, slow_mo=250)
+# page = get_page_object(browser)
+# goto_url(TRIP_ADVISOR_HOMEPAGE, page)
+
+places = get_file_content("places.txt")
